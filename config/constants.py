76a62a9dba4d28bc74c69c2485f46ffdeb08ca59
William Goh <wirriamm@gmail.com>
2020-06-08 14:12:17 +0800

    constants file checks sys.platform to differentiate mac and linux constants

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,13 @@
 from os.path import realpath, dirname
+import sys
 
-DATA_PATH = "/Users/wirriamm/marek_lab/plants-pipeline/data-local"
-ASPERA_SSH_KEY = "/Users/wirriamm/Applications/Aspera CLI/etc/asperaweb_id_dsa.openssh" # For MacOS
-# ASPERA_SSH_KEY = "/home/.aspera/cli/Aspera CLI/etc/asperaweb_id_dsa.openssh" # For Linux
+if sys.platform == 'linux':
+    DATA_PATH = "/home/data1/will_data/pipeline-data"
+    ASPERA_SSH_KEY = "/home/workstation/.aspera/cli/etc/asperaweb_id_dsa.openssh" # For Linux
+elif sys.platform == 'darwin':
+    DATA_PATH = "/Users/wirriamm/marek_lab/plants-pipeline/data-local"
+    ASPERA_SSH_KEY = "/Users/wirriamm/Applications/Aspera CLI/etc/asperaweb_id_dsa.openssh" # For MacOS
+else:
+    print("Please manually set your constants")
+    DATA_PATH = input("DATA_PATH: ")
+    ASPERA_SSH_KEY = input("ASPERA_SSH_KEY: ")
