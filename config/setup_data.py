5dfff4ebd8361e874d0a2ae489c5620d4ce3d9b7
William Goh <wirriamm@gmail.com>
2020-07-07 13:40:29 +0800

    refactored script to setup data directories

--- config/setup_data.py
+++ config/setup_data.py
@@ -6,23 +6,38 @@ parser.add_argument('-p', '--path', nargs=1, metavar='data_path',
                     help='Enter the path of the directory where the data directories for this project is to be created',
                     dest='data_path', type=str, required=True)
 args = parser.parse_args()
-DATA_PATH = args.data_path[0] + '/pipeline-data'
+DATA_PATH = args.data_path[0] + '/pipeline-data/'
 
-os.makedirs(f"{DATA_PATH}/download/bash-tmp", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/cds", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/idx", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/fastq-tmp", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/kallisto-tmp", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/kallisto-out", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/logs/initiation", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/logs/runtime", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/download/logs/progress", exist_ok=True)
+# @HERE: TO CONFIGURE
+dests = {
+    'download': [
+        'cds',
+        'idx',
+        'bash-tmp',
+        'fastq-tmp',
+        'kallisto-tmp',
+        'kallisto-out',
+        'logs/initiation',
+        'logs/runtime',
+        'logs/progress'
+    ],
+    'preprocess': [
+        'out/ena-runtables',
+        'out/sra-runtables',
+        'out/ena-annotations',
+        'out/sra-annotations'
+    ],
+    'postprocess': [
+        ''
+    ]
+}
 
-os.makedirs(f"{DATA_PATH}/preprocess/out/ena-runtables", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/preprocess/out/sra-runtables", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/preprocess/out/ena-annotations", exist_ok=True)
-os.makedirs(f"{DATA_PATH}/preprocess/out/sra-annotations", exist_ok=True)
+def makedirs(dest):
+    os.makedirs(f"{DATA_PATH}{dest}", exist_ok=True)
 
-os.makedirs(f"{DATA_PATH}/postprocess", exist_ok=True)
+for p, c in dests.items():
+    for dest in c:
+        makedirs(f"{p}/{dest}")
+        print(f"Created: {p}/{dest}")
 
-print("Created directories for pipeline data repository")
+print("🚀 Created directories for pipeline data repository")
