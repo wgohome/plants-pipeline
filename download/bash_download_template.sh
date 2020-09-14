83be4c6c898da2e478e76727276df925c1814597
William Goh <wirriamm@gmail.com>
2020-09-14 18:37:39 +0800

    writes tab as '\t' for compatibility with writing on linux systems with xargs, but will fail with macOS

--- download/bash_download_template.sh
+++ download/bash_download_template.sh
@@ -1,4 +1,4 @@
-echo -e $(date +%Y%m%d-%H%M%S"\t{runid}") >> {init_log_path};
+echo -e $(date +%Y%m%d-%H%M%S"\\t{runid}") >> {init_log_path};
 ascp_start=$(date +%s);
 {offset}
 timeout 360s ascp -QT -k1 -P33001 -l 300m {ascp_limit_tag} -i "{ASPERA_SSH_KEY}" era-fasp@fasp.sra.ebi.ac.uk:vol1/fastq/{route} '{fastq_path}';
@@ -6,7 +6,7 @@ ascp_time=$(echo $(date +%s) - $ascp_start | bc);
 sleep 5;
 if [ -f {fastq_path}.aspx ];
   then rm {fastq_path}*;
-  echo -e $(date +%Y%m%d-%H%M%S"\t{runid}\tfailed\tfailed\t{layout}") >> {runtime_log_path};
+  echo -e $(date +%Y%m%d-%H%M%S"\\t{runid}\\tfailed\\tfailed\\t{layout}") >> {runtime_log_path};
 elif [ -f {fastq_path} ];
   then kallisto_start=$(date +%s);
   kallisto quant -i {idx_path} -t {threads} -o {kal_tmp} --single -l 200 -s 20 {fastq_path};
@@ -19,7 +19,7 @@ elif [ -f {fastq_path} ];
   zip -r {kal_out}{runid}.zip {runid};
   cd "{parent_module}";
   rm -r {kal_tmp};
-  echo -e $(date +%Y%m%d-%H%M%S"\t{runid}\t$ascp_time\t$kallisto_time\t{layout}") >> {runtime_log_path};
+  echo -e $(date +%Y%m%d-%H%M%S"\\t{runid}\\t$ascp_time\\t$kallisto_time\\t{layout}") >> {runtime_log_path};
 else
-  echo -e $(date +%Y%m%d-%H%M%S"\t{runid}\tfailed\tfailed\t{layout}") >> {runtime_log_path};
+  echo -e $(date +%Y%m%d-%H%M%S"\\t{runid}\\tfailed\\tfailed\\t{layout}") >> {runtime_log_path};
 fi
