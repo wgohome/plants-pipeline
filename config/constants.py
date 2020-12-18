72d9624ff786c77e66f5c61dfe433ebdec529fa5
William Goh <wirriamm@gmail.com>
2020-12-18 16:29:57 +0800

    added commented out aspera ssh key for wallace

--- config/constants.py
+++ config/constants.py
@@ -3,7 +3,8 @@ import sys
 
 # Linux
 DATA_PATH = "/Users/wirriamm/marek_lab/plants-pipeline/pipeline-data/"
-ASPERA_SSH_KEY = "/home/workstation/.aspera/cli/etc/asperaweb_id_dsa.openssh"
-# ASPERA_SSH_KEY = "/Users/wirriamm/Applications/Aspera CLI/etc/asperaweb_id_dsa.openssh"
+# ASPERA_SSH_KEY = "/home/will0046/.aspera/cli/etc/asperaweb_id_dsa.openssh"
+# ASPERA_SSH_KEY = "/home/workstation/.aspera/cli/etc/asperaweb_id_dsa.openssh"
+ASPERA_SSH_KEY = "/Users/wirriamm/Applications/Aspera CLI/etc/asperaweb_id_dsa.openssh"
 
 __all__ = ['DATA_PATH', 'ASPERA_SSH_KEY']
