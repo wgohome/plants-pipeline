efd74a18e18f97d18a4720ad36dcf7d1d653a336
William Goh <wirriamm@gmail.com>
2020-12-29 18:49:21 +0800

    allow choice of method to pull tpm tables

--- main/update_all_tpm_matrices.py
+++ main/update_all_tpm_matrices.py
@@ -29,9 +29,13 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--newonly', action='store_true', default=False,
                         help='Include this optional tag if you only want to process species whose matrices does not yet exist.',
                         dest='newonly', required=False)
+    parser.add_argument('-m', '--method', choices=[1, 2, 3], default=3,
+                        help='Include this optional tag to specify the method to use to pull the tpm values.',
+                        dest='method', type=int, required=False)
     args = parser.parse_args()
     threads = args.threads
     newonly = args.newonly
+    method = args.method
 
 taxids = run_job.get_valid_jobs()
 if newonly:
@@ -39,12 +43,18 @@ if newonly:
     existing_taxids = [int(re.findall("taxid(\d{4,6})", file)[0]) for file in files if "taxid" in file]
     taxids = list(set(taxids) - set(existing_taxids))
 
+# Choose method
+methods = { 1: pull_tpm_matrix.write_tpm_matrix,
+            2: pull_tpm_matrix.write_tpm_matrix2,
+            3: pull_tpm_matrix.write_tpm_matrix3 }
+write_tpm = methods[method]
+
 if threads == 0:
     for taxid in taxids:
         pull_tpm_matrix.write_tpm_matrix3(taxid)
 elif threads > 0:
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        futures = [executor.submit(pull_tpm_matrix.write_tpm_matrix3, taxid) for taxid in taxids]
+        futures = [executor.submit(write_tpm, taxid) for taxid in taxids]
         results = []
         for f in concurrent.futures.as_completed(futures):
             results.append(f.result())
